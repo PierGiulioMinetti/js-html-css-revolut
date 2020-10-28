0fa42b149e64e96fff5f4e630a54f0bd22a5abe7
Pier Giulio Minetti <pg.minetti@gmail.com>
2020-10-28 16:35:31 +0100

    Menu Prodotti

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function() { 
+
+// referenze
+var aProdotti = $('.prodotti-class > a');
+var dropProdotti = $('.prodotti-class > .drop-down');
+
+var aAzienda = $('.li-drop-azienda > a');
+var dropAzienda = $('.li-drop-azienda > azienda-drop-down');
+
+// eventi
+aProdotti.click(function()  {
+    console.log('stai cliccando su prodotti');
+    $(this).next('.drop-down').toggle();
+});
+
+aAzienda.click(function() {
+    console.log('stai cliccando su azienda');
+});
+
+
+
+
+ });

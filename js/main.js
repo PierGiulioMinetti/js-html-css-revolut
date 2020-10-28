d0f63654c926925a7d344c84ff12bcc78f25241a
Pier Giulio Minetti <pg.minetti@gmail.com>
2020-10-28 18:00:26 +0100

    Fixing Menu

--- js/main.js
+++ js/main.js
@@ -2,32 +2,12 @@
 $(document).ready(function() { 
 
 // referenze
-var aProdotti = $('.prodotti-class > a');
-var dropProdotti = $('.prodotti-class > .drop-down');
+var dropLinks = $('.with-d-down > a');
+var dropMenu = $('.with-d-down > .drop-down');
 
-var aAzienda = $('.li-drop-azienda > a');
-var dropAzienda = $('.li-drop-azienda > azienda-drop-down');
-
-// eventi
-aProdotti.click(function()  {
-    console.log('stai cliccando su prodotti');
-
+// visibilità
+dropLinks.click(function() {
     $(this).next('.drop-down').toggle();
-});
-
-aAzienda.click(function() {
-    console.log('stai cliccando su azienda');
-
-    var actualAzienda = $(this).next('.azienda-drop-down');
-
-    dropAzienda.not(actualAzienda).hide();
-
-    actualAzienda.toggle();
-
-    // $(this).next('.azienda-drop-down').toggle();
-});
-
-
-
+})
 
  });
